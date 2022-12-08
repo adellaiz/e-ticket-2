c349b67c6a31eae00b69ba76c182c9facd7eef17
Ali Akbar <63591934+ali5alkaf5@users.noreply.github.com>
2022-12-08 11:35:25 +0700

    Horeeee

---
+++ app/Mail/OrderTicketMail.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Mail;
+
+use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class OrderTicketMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $data;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        //
+        $this->data = $data;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Order Ticket Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'attachments.order-pdf',
+            with: array_merge($this->data, ['email' => true]),
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [
+            Attachment::fromData(
+                data: fn() => Pdf::loadView('attachments.order-pdf', $this->data)->output(),
+                name: "order_" . $this->data['order']->booking_code . ".pdf",
+            ),
+
+        ];
+    }
+}
